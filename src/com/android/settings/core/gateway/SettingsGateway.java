cad08e1b5409c5afcd8698111703c0097d913ea0
kenway214 <kenway214@outlook.com>
2026-08-06 09:57:01 +0000

    Settings: Implement AppLock settings interface
    Change-Id: I6f4231deb6385cbde4da73c3562d79633f969868
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -195,6 +195,7 @@ import com.android.settings.regionalpreferences.TemperatureUnitFragment;
 import com.android.settings.safetycenter.MoreSecurityPrivacyFragment;
 import com.android.settings.safetycenter.ui.SafetyCenterFragment;
 import com.android.settings.safetycenter.ui.SafetyCenterQsFragment;
+import com.android.settings.security.AppLockSettingsFragment;
 import com.android.settings.security.ContentProtectionPreferenceFragment;
 import com.android.settings.security.LockscreenDashboardFragment;
 import com.android.settings.security.MemtagPage;
@@ -427,6 +428,7 @@ public class SettingsGateway {
             ZenModeCallsFragment.class.getName(),
             ContactsStorageSettings.class.getName(),
             ContentProtectionPreferenceFragment.class.getName(),
+            AppLockSettingsFragment.class.getName(),
             MagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
             BundlePreferenceFragment.class.getName(),
